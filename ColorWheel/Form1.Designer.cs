a568ffdcfd5d805d9488a64ca010866c76efbd53
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 22:33:22 +0200

    Add project files.

---
+++ ColorWheel/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ColorWheel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtr = new System.Windows.Forms.TextBox();
+            this.txtg = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 373);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(468, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw the wheel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtr
+            // 
+            this.txtr.Location = new System.Drawing.Point(96, 394);
+            this.txtr.Name = "txtr";
+            this.txtr.Size = new System.Drawing.Size(100, 20);
+            this.txtr.TabIndex = 2;
+            // 
+            // txtg
+            // 
+            this.txtg.Location = new System.Drawing.Point(96, 421);
+            this.txtg.Name = "txtg";
+            this.txtg.Size = new System.Drawing.Size(100, 20);
+            this.txtg.TabIndex = 3;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(96, 448);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "r";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "g";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "b";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 490);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txtg);
+            this.Controls.Add(this.txtr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "color wheel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtr;
+        private System.Windows.Forms.TextBox txtg;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ColorWheel/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ColorWheel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Pen p;
+        Brush b;
+        Font f;
+        int dim = 10;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            //p = new Pen(Color.Black, dim);
+            p = new Pen(Color.Black, 1);
+            b = new SolidBrush(Color.Black);
+            f = Font;
+
+        }
+
+        public void draw()
+        {
+            for (int pas = 0; pas < 23; pas++)
+            {
+
+                for (int i = 0; i < 256; i += dim)
+                {
+                    for (int j = 0; j < 256; j += dim)
+                    {
+                        for (int k = 0; k < 256; k += dim)
+                        {
+                            if(pas==0)
+                                p.Color = Color.FromArgb(k, i, j);
+                            if (pas == 1)
+                                p.Color = Color.FromArgb(k, j, i);
+                            if (pas == 2)
+                                p.Color = Color.FromArgb(i, k, j);
+                            if (pas == 3)
+                                p.Color = Color.FromArgb(i, j, k);
+                            if (pas == 4)
+                                p.Color = Color.FromArgb(j, i, k);
+                            if (pas == 5)
+                                p.Color = Color.FromArgb(j, k, i);
+                            if (pas == 6)
+                                p.Color = Color.FromArgb(k, i, 0);
+                            if (pas == 7)
+                                p.Color = Color.FromArgb(k, j, 0);
+                            if (pas == 8)
+                                p.Color = Color.FromArgb(i, k, 0);
+                            if (pas == 9)
+                                p.Color = Color.FromArgb(i, j, 0);
+                            if (pas == 10)
+                                p.Color = Color.FromArgb(j, i, 0);
+                            if (pas == 11)
+                                p.Color = Color.FromArgb(j, k, 0);
+                            if (pas == 12)
+                                p.Color = Color.FromArgb(0,k, i);
+                            if (pas == 13)
+                                p.Color = Color.FromArgb(0,k, j);
+                            if (pas == 14)
+                                p.Color = Color.FromArgb(0,i, k);
+                            if (pas == 15)
+                                p.Color = Color.FromArgb(0,i, j);
+                            if (pas == 16)
+                                p.Color = Color.FromArgb(0,j, i);
+                            if (pas == 17)
+                                p.Color = Color.FromArgb(0,j, k);
+                            if (pas == 18)
+                                p.Color = Color.FromArgb(k, 0, j);
+                            if (pas == 19)
+                                p.Color = Color.FromArgb(k, 0, i);
+                            if (pas == 20)
+                                p.Color = Color.FromArgb(i, 0, j);
+                            if (pas == 21)
+                                p.Color = Color.FromArgb(i, 0, k);
+                            if (pas == 22)
+                                p.Color = Color.FromArgb(j, 0, k);
+                            if (pas == 23)
+                                p.Color = Color.FromArgb(j, 0, i);
+
+                           // g.DrawEllipse(p, j, i, dim, dim);
+                            //g.DrawEllipse(p, pas * (j / dim), pas * (i / dim), 5, 5);
+                            g.FillEllipse(new SolidBrush(p.Color), j, i, dim, dim);
+                            txtr.Text = i.ToString();
+                            txtg.Text = j.ToString();
+                            txtb.Text = k.ToString();
+                            //Thread.Sleep(1);
+                            txtr.Refresh();
+                            txtg.Refresh();
+                            txtb.Refresh();
+                        }
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw();
+        }
+    }
+}
